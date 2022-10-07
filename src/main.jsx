fee5d31595db9316c2d9caf082101a7caabc8f5d
erbaafidotama <erbaafidotama13@gmail.com>
2022-10-07 20:18:26 +0700

    remake code for react router and render with react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import Root from "./routes/root";
 import Contact from "./routes/contact";
 import ErrorPage from "./error-page";
-import WargaIndex from "./routes/Warga/index"
-import LoginIndex from "./routes/Login/index"
+import WargaIndex from "./routes/Warga/index";
+import LoginIndex from "./routes/Login/index";
+import HomeIndex from "./routes/Home/index";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
       {
         path: "login",
         element: <LoginIndex />,
+      },
+      {
+        path: "home",
+        element: <HomeIndex />,
       },
       {
         path: "contacts/:contactId",
@@ -31,8 +36,11 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <RouterProvider router={router}>
+    <App />
+  </RouterProvider>
 );

eb8633b20148c8302eb79e289f2571bdb1c6bb6a
erbaafidotama <erbaafidotama13@gmail.com>
2022-10-07 20:20:37 +0700

    remake ccode for example template

--- src/routes/Warga/index.jsx
+++ src/routes/Warga/index.jsx
@@ -1,28 +1,57 @@
 import TextField from "@mui/material/TextField";
-import { useEffect } from "react";
+import { useEffect, useCallback, useState } from "react";
 import Typography from "@mui/material/Typography";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Divider from "@mui/material/Divider";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
 
 import Table from "../../components/Table";
+import ButtonList from "../../components/ButtonList";
 
 import { useWargaStore } from "./store";
 import { useLoginStore } from "../Login/store";
 
 const WargaIndex = () => {
-  const bears = useWargaStore((state) => state.bears);
-  const increasePopulation = useWargaStore((state) => state.increasePopulation);
-  const removeAllBears = useWargaStore((state) => state.removeAllBears);
-  const fetchPeople = useWargaStore((state) => state.fetchPeople);
-  const listPeople = useWargaStore((state) => state.people);
-  const jwtToken = useLoginStore((state) => state.token);
+  const posts = useWargaStore((state) => state.posts);
+  const fetchPost = useWargaStore((state) => state.fetchPost);
+
+  const [openModalTambah, setOpenModalTambah] = useState(false);
 
   useEffect(() => {
-    fetchPeople(jwtToken);
+    console.log("SINIII");
+    fetchPost();
   }, []);
 
-  console.log("listPeople", listPeople);
-  console.log("jwtToken", jwtToken);
+  const doOpenModalTambah = () => {
+    setOpenModalTambah(!openModalTambah);
+  };
+
+  const buttonList = [
+    {
+      typeButton: "add",
+      onClick: doOpenModalTambah,
+      label: "Add People",
+    },
+    {
+      typeButton: "edit",
+      onClick: doOpenModalTambah,
+      label: "Edit People",
+    },
+    {
+      typeButton: "delete",
+      onClick: doOpenModalTambah,
+      label: "Delete People",
+    },
+  ];
+
   return (
     <div>
       <Breadcrumbs aria-label="breadcrumb">
@@ -31,42 +60,72 @@ const WargaIndex = () => {
       </Breadcrumbs>
       <Divider />
       <br />
-      {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
-      {listPeople.data && <DataTable dataTable={listPeople} />}
+      <ButtonList buttonList={buttonList} />
+      <br />
 
-      <h1>{bears} around here ...</h1>
-      <button onClick={increasePopulation}>one up</button>
-      <button onClick={removeAllBears}>reset</button>
+      {posts.data && <DataTable dataTable={posts} />}
+
+      <Dialog
+        open={openModalTambah}
+        // keepMounted
+        onClose={doOpenModalTambah}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>Tambah Data Warga</DialogTitle>
+        <Divider />
+        <DialogContent>
+          <Stack direction="row" spacing={2}>
+            <TextField
+              id="outlined-basic"
+              label="Nama Lengkap"
+              variant="outlined"
+            />
+            <TextField id="outlined-basic" label="NIK" variant="outlined" />
+          </Stack>
+          <Stack direction="row" spacing={2}>
+            <TextField id="outlined-basic" label="Warga" variant="outlined" />
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={doOpenModalTambah}>Close</Button>
+          <Button onClick={doOpenModalTambah}>Save</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
 
 const columns = [
   {
-    name: "nama_lengkap",
-    header: "Nama Lengkap",
-    minWidth: 130,
-    defaultFlex: 2,
+    name: "id",
+    header: "No.",
+    maxWidth: 70,
   },
   {
-    name: "nik",
-    header: "NIK",
-    minWidth: 90,
+    name: "title",
+    header: "Title",
+    defaultFlex: 1,
   },
   {
-    name: "no_kk",
-    header: "No. KK",
-    minWidth: 90,
+    name: "body",
+    header: "Body",
+    defaultFlex: 1,
   },
 ];
 
 function DataTable(props) {
-  console.log("props.dataTable", props.dataTable);
+  const [selected, setSelected] = useState(null);
+  const onSelectionChange = useCallback(({ selected, e }) => {
+    console.log("e", e);
+    setSelected(selected);
+  }, []);
+  console.log("selected", selected);
   return (
-    <div >
+    <div>
       <Table
         columnsTable={columns}
         dataTable={props.dataTable?.data}
+        onRowClick={onSelectionChange}
       />
     </div>
   );
